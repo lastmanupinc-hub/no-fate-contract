9883dfa29fdd7e359ea9132f477be4d28a3c3dae
PMD System <pmd@system.local>
2025-12-17 15:07:17 -0600

    Canon compliance: withdraw authority-enabling artifacts

--- src/engines/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * IRS No-Fate Tax Analysis Suite - Engine Exports
- */
-
-export { analyze_completeness } from "./completeness";
-export { classify_income } from "./income";
-export { verify_deduction_docs } from "./deduction";
-export { route_business_filing } from "./filing";
-export { analyze_notice } from "./notice";
